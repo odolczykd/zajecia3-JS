1b5dc1c71a781e5387ce307a6a423b73ec996afc
Dawid Odolczyk <dawid.odolczyk@gmail.com>
2022-11-29 09:06:48 +0100

    ex1

---
+++ zad1/functions.js
@@ -0,0 +1,11 @@
+function isOdd(x){
+    return x%2 !== 0;
+}
+
+const isEven = x => x%2 === 0;
+
+console.log(isOdd(5));
+console.log(isOdd(8));
+console.log(isEven(12));
+console.log(isEven(33));
+
